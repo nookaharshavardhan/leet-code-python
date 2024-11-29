9054ed326c23d6f85b802692a51532bc9a8ef73e
harsha <nookaharshavardhan10@gmail.com>
2024-11-29 18:53:00 +0530

    Two Sum

--- q0001.py
+++ q0001.py
@@ -3,11 +3,7 @@ class Solution(object):
         nums_dictonary={} #to store the key values pairs of nums and indexes
         for i,num in enumerate(nums):
             x=target-num
-            if x in nums_dictonary:
+            if x in nums_dictonary:#checks in dict which store alredy iterated num
                 return [nums_dictonary[x],i]
             nums_dictonary[num]=i
         return []
-
-lst=[1,2,3,5,6,7,]
-hi=Solution(lst,9)
-print(hi.twoSum(lst,9))
